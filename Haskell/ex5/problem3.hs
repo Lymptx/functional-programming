3ef39951c3327b901f449087c8445794a0b059ea
BenniLaptop <benjam.winkler@gmail.com>
2026-06-15 09:42:21 +0200

    p3

---
+++ Haskell/ex5/problem3.hs
@@ -0,0 +1,11 @@
+data Tree = Nil | Node Int Tree Tree
+
+layer :: Tree -> Int -> [Int]
+layer Nil _           = []
+layer _ t | t < 0    = []
+layer (Node x l r) 0 = [x]
+layer (Node x l r) t = layer l (t-1) ++ layer r (t-1)
+
+cut :: Tree -> Int -> Int -> [[Int]]
+cut _ m n | m > n = []
+cut b m n         = [layer b t | t <- [m..n]]
